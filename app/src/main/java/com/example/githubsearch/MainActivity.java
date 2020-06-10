b270dc8640ca60b10015aa40b501411197894ccb
Samip <samipgnyawali15@gmail.com>
2020-06-10 13:40:26 +0545

    first commit

---
+++ app/src/main/java/com/example/githubsearch/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.githubsearch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String SEARCHQUERY_URL = "searchUrl";
+    private static final String SEARCH_JSON_DATA = "searchJsonData";
+    private EditText mSearchBoxEditText;
+    private TextView mUrlDisplayTextView;
+    private TextView mSearchResultsTextView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        String[] dummydata = {
+//                "Today, May 17 - Clear - 17°C / 15°C",
+//                "Tomorrow - Cloudy - 19°C / 15°C",
+//                "Thursday - Rainy- 30°C / 11°C",
+//                "Friday - Thunderstorms - 21°C / 9°C",
+//                "Saturday - Thunderstorms - 16°C / 7°C",
+//                "Sunday - Rainy - 16°C / 8°C",
+//                "Monday - Partly Cloudy - 15°C / 10°C",
+//                "Tue, May 24 - Meatballs - 16°C / 18°C",
+//                "Wed, May 25 - Cloudy - 19°C / 15°C",
+//                "Thu, May 26 - Sto",
+//        "Fri, May 27 - Hurricane - 21°C / 9°C",
+//                "Sat, May 28 - Meteors - 16°C / 7°C",
+//                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+//                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+//        };
+        mSearchBoxEditText = findViewById(R.id.et_search_box);
+
+        mUrlDisplayTextView = findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+
+        if (savedInstanceState!=null){
+            String queryUrl = savedInstanceState.getString(SEARCHQUERY_URL);
+            String queryJsondata = savedInstanceState.getString(SEARCH_JSON_DATA);
+            mUrlDisplayTextView.setText(queryUrl);
+            mSearchResultsTextView.setText(queryJsondata);
+        }
+
+    }
+
+    private void makeGithubSearchQuery() {
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+        new GithubQueryTask().execute(githubSearchUrl);
+    }
+
+    private void showJsonDataView() {
+        // First, make sure the error is invisible
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        // Then, make sure the JSON data is visible
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        // First, hide the currently visible data
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        // Then, show the error
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        // COMPLETED (26) Override onPreExecute to set the loading indicator to visible
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                // COMPLETED (17) Call showJsonDataView if we have valid, non-null results
+                showJsonDataView();
+                mSearchResultsTextView.setText(githubSearchResults);
+            } else {
+                // COMPLETED (16) Call showErrorMessage if the result is null in onPostExecute
+                showErrorMessage();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            makeGithubSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //-----------------Saving Instance
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String queryUrl = mUrlDisplayTextView.getText().toString();
+        String jsondata = mSearchResultsTextView.getText().toString();
+        outState.putString(SEARCHQUERY_URL,queryUrl);
+        outState.putString(SEARCH_JSON_DATA,jsondata);
+    }
+}
